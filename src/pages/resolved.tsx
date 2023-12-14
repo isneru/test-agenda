9a2b922d2761bb601dafc10097ca2c0ddcd4a5a2
Diogo Nogueira <diogoclanogueira@gmail.com>
2023-12-14 13:13:24 +0000

    fix: 2 pages instead of 1 for better UI approach

--- src/pages/resolved.tsx
+++ src/pages/resolved.tsx
@@ -1,12 +1,10 @@
 import { Test } from '@components'
-import { useTestProvider } from '@lib/providers/new-test'
 import { api } from '@utils/api'
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Home() {
+export default function Resolved() {
   const { data: allTests } = api.test.getAll.useQuery()
-  const { toggleModal } = useTestProvider()
 
   return (
     <>
@@ -16,21 +14,20 @@ export default function Home() {
         <link rel="icon" href="/cex.png" />
       </Head>
       <div className="flex min-h-screen flex-col py-10 gap-4">
-        <header className="mb-4 flex flex-col items-center justify-center gap-4">
-          <h1 className="text-center text-6xl font-bold">CeX Test Agenda</h1>
-          <button onClick={toggleModal} className="rounded-xl bg-red-800 p-2">
-            Registar novo teste
-          </button>
+        <header className="flex flex-col items-center justify-center mb-[72px]">
+          <h1 className="text-center text-6xl font-bold">
+            CeX Test Agenda (Resolvidos)
+          </h1>
         </header>
         <main className="mx-auto grid grid-flow-row-dense grid-cols-1 gap-6 lg:grid-cols-2 2xl:grid-cols-3">
-          {allTests?.unresolvedTests?.map(test => (
+          {allTests?.resolvedTests?.map(test => (
             <Test key={test.id} test={test} />
           ))}
         </main>
         <Link
-          href="/resolved"
+          href="/"
           className="absolute bottom-5 left-5 flex items-center justify-center rounded-full border border-red-800/10 bg-red-800/10 px-3 py-1 text-red-300 transition-colors hover:bg-red-800/20">
-          Ver resolvidos
+          Voltar
         </Link>
       </div>
     </>
